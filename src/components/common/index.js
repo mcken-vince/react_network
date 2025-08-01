4c89757f5a5b52dc21af94750c139636f0f0cb66
mcken-vince <mcken.vince@gmail.com>
2025-08-01 09:10:04 -0600

    Export common components

---
+++ src/components/common/index.js
@@ -0,0 +1,4 @@
+// Barrel export for all common components
+export { default as Avatar } from "./Avatar";
+export { default as Card } from "./Card";
+export { default as UserInfo } from "./UserInfo";
